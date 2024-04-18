6f15d7dd643c6953e24a0182b0e984d481c882dd
nigam <nigammishra826@gmail.com>
2024-04-18 13:04:15 +0530

    doodle image add

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -29,6 +29,7 @@ import { Fade, Slide, Bounce, Zoom } from "react-awesome-reveal";
 import location from '../image/location.gif'
 import review from '../image/Review.gif'
 import owner from '../image/owner.gif'
+import doodleimage from '../image/doodles.png'
 
 
 const Home = () => {
@@ -761,11 +762,15 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <css-doodle style={{ position: 'absolute', display: 'flex', alignItems: 'center', }}>
+        <div className="doodle">
+        <css-doodle style={{ position: 'absolute', display: 'flex', alignItems: 'center', }} >
           <style dangerouslySetInnerHTML={{ __html: "\n  --color: #51eaea, #fffde1, #ff9d76, #FB3569;\n\n  @grid: 30x1 / 100% 78vh / #270f34; \n  \n  :container {\n    perspective: 30vmin;\n    --deg: @p(-180deg, 180deg);\n  }\n  \n  :after, :before {\n    content: '';\n    background: @p(--color); \n    @place: @r(100%) @r(100%);\n    @size: @r(6px);\n    @shape: heart;\n  }\n\n  @place: center;\n  @size: 18vmin; \n\n  box-shadow: @m2(0 0 50px @p(--color));\n  background: @m100(\n    radial-gradient(@p(--color) 50%, transparent 0) \n    @r(-20%, 120%) @r(-20%, 100%) / 1px 1px\n    no-repeat\n  );\n\n  will-change: transform, opacity;\n  animation: scale-up 26s linear infinite;\n  animation-delay: calc(-12s / @I * @i);\n\n  @keyframes scale-up {\n    0%, 95.01%, 100% {\n      transform: translateZ(0) rotate(0);\n      opacity: 0;\n    }\n    10% { \n      opacity: 1; \n    }\n    95% {\n      transform: \n        translateZ(35vmin) rotateZ(var(--deg));\n    }\n  }\n" }} />
         </css-doodle>
-
-
+        </div>
+      
+      <div className='doodleimage'>
+      <img src={doodleimage} className="img-fluid" style={{height:'500px'}}  />
+      </div>
       </>
       <>
         <div className="container p-t180" style={{paddingTop:'600px'}}>
