0723b41b683485634cdb02edc6c5567cd0e177f2
nigam <nigammishra826@gmail.com>
2024-04-22 20:35:54 +0530

    background changed in doodles section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -997,14 +997,14 @@ const Home = () => {
               </div>
             </div>
 
-            <div className="">
+            <div className="doodle">
               <css-doodle style={{ position: 'absolute', display: 'flex', alignItems: 'center', }} >
                 <style dangerouslySetInnerHTML={{ __html: "\n  --color: #51eaea, #fffde1, #ff9d76, #FB3569;\n\n  @grid: 30x1 / 100% 78vh / #270f34; \n  \n  :container {\n    perspective: 30vmin;\n    --deg: @p(-180deg, 180deg);\n  }\n  \n  :after, :before {\n    content: '';\n    background: @p(--color); \n    @place: @r(100%) @r(100%);\n    @size: @r(6px);\n    @shape: heart;\n  }\n\n  @place: center;\n  @size: 18vmin; \n\n  box-shadow: @m2(0 0 50px @p(--color));\n  background: @m100(\n    radial-gradient(@p(--color) 50%, transparent 0) \n    @r(-20%, 120%) @r(-20%, 100%) / 1px 1px\n    no-repeat\n  );\n\n  will-change: transform, opacity;\n  animation: scale-up 26s linear infinite;\n  animation-delay: calc(-12s / @I * @i);\n\n  @keyframes scale-up {\n    0%, 95.01%, 100% {\n      transform: translateZ(0) rotate(0);\n      opacity: 0;\n    }\n    10% { \n      opacity: 1; \n    }\n    95% {\n      transform: \n        translateZ(35vmin) rotateZ(var(--deg));\n    }\n  }\n" }} />
               </css-doodle>
             </div>
 
             <div className='doodleimage'>
-
+              
               <div class="row" >
                 <div class="col-md-4" style={{ display: 'flex', justifyContent: 'center' }}>
                   
