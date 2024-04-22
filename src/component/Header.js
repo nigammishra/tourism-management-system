a6a2a21a9564f22d0141d399a1bdb8fb3045b92c
nigam <nigammishra826@gmail.com>
2024-04-22 11:06:57 +0530

    some changes by pupun

--- src/component/Header.js
+++ src/component/Header.js
@@ -23,11 +23,11 @@ const Header = () => {
         <section className="nav-bar" id='header'>
           <div className="logo"><img className="" src={img} style={{ height: '60px', width: '150px' }} alt /></div>
           <ul className="menu1">
-            <li><Link to="/tourism-management-system">home</Link></li>
-            <li><Link to="/about-us">about</Link></li>
-            <li><Link to="/Package">Package</Link></li>
-            <li><Link to="/Gallery">gallery</Link></li>
-            <li><Link to="/Contact">Contact us</Link></li>
+            <li><Link className='underline' to="/tourism-management-system">home</Link></li>
+            <li><Link className='underline' to="/about-us">about</Link></li>
+            <li><Link className='underline' to="/Package">Package</Link></li>
+            <li><Link className='underline' to="/Gallery">gallery</Link></li>
+            <li><Link className='underline' to="/Contact">Contact us</Link></li>
           </ul>
         </section>
         <div id="header1">
