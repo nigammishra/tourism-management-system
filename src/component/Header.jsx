14322a594650473f98a569bad5ab0cf56b3fdbfd
nigam <nigammishra826@gmail.com>
2024-05-06 23:18:59 +0530

    sidenav automatically close when we click any link button

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -41,27 +41,22 @@ const Header = () => {
         <div id="header1">
           <div id="mySidenav" className="sidenav" >
             <Link
-              to="javascript:void(0)"
+              // to="javascript:void(0)"
               className="closebtn"
               onClick={closeNav}
             >
               ×
             </Link>
             <ul className="">
-              <li><Link to="/tourism-management-system">home</Link></li>
-              <li><Link to="/about-us">about</Link></li>
-              <li><Link to="/Package">Package</Link></li>
-              <li><Link to="/Gallery">gallery</Link></li>
-              <li><Link to="/Contact">Contact us</Link></li>
+              <li><Link to="/tourism-management-system"  onClick={closeNav}>home</Link></li>
+              <li><Link to="/about-us"  onClick={closeNav}>about</Link></li>
+              <li><Link to="/Package"  onClick={closeNav}>Package</Link></li>
+              <li><Link to="/Gallery"  onClick={closeNav}>gallery</Link></li>
+              <li><Link to="/Contact"  onClick={closeNav}>Contact us</Link></li>
             </ul>
           </div>
           <div id="main">
-            {/* <span
-              style={{ fontSize: 30, cursor: "pointer" }}
-              onclick="openNav()"
-            >
-              ☰ open
-            </span> */}
+           
             <span style={{ fontSize: 30, cursor: "pointer", float: 'right' }} onClick={openNav}>
               <img
                 src={img}
